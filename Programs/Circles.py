9e44875f5b33fb49a7e22a0fe2b1df9752e4291b
sruthim <manojsruthi10@gmail.com>
2020-09-20 17:30:32 -0700

    Added Circles program.

---
+++ Programs/Circles.py
@@ -0,0 +1,4 @@
+# Write a Python program which accepts the radius of a circle from the user and compute the area.
+r = int(input("enter the radius of a random circle"))
+area = 3.14 * r**2
+print("area of that circle is", area)
